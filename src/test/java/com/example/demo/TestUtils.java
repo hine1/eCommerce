b417727fd5c726503ec344072f7418adec0d43ac
Hien Truong <hine113@gmail.com>
2021-04-06 11:08:37 -0500

    fix bugs

--- src/test/java/com/example/demo/TestUtils.java
+++ src/test/java/com/example/demo/TestUtils.java
@@ -15,7 +15,7 @@ public class TestUtils {
 
         try {
             Field f = target.getClass().getDeclaredField(fieldName);
-            if (!f.canAccess(target)){
+            if (f.get(target) == null){
                 f.setAccessible(true);
                 wasPrivate = true;
             }
